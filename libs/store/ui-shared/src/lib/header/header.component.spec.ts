186377ac07f8f2f1ba900a3ec2cca6153d7ac94f
Jasper Catthoor <jcatthoor@inimco.com>
2023-03-23 14:47:01 +0100

    fix tests

--- libs/store/ui-shared/src/lib/header/header.component.spec.ts
+++ libs/store/ui-shared/src/lib/header/header.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import {MatToolbar} from "@angular/material/toolbar";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +8,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent],
+      declarations: [HeaderComponent, MatToolbar],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
